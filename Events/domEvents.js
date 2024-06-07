2f3186f5a3031f4d896f03e54020f59d5719d1ec
Melissa Carter <mkd.princess@gmail.com>
2024-06-06 23:01:42 -0500

    dom event changes

--- Events/domEvents.js
+++ Events/domEvents.js
@@ -1,8 +1,7 @@
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
-    // TODO: CLICK EVENT FOR DELETING A WORD
+    //  CLICK EVENT FOR DELETING A WORD
     if (e.target.id.includes('delete-word')) {
-      // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
         console.warn('CLICKED DELETE WORD', e.target.id);
         const [, firebaseKey] = e.target.id.split('--');
@@ -12,12 +11,12 @@ const domEvents = () => {
         });
       }
     }
-    // TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A WORD
+    // CLICK EVENT FOR SHOWING FORM FOR ADDING A WORD
     if (e.target.id.includes('add-WORD-btn')) {
       console.warn('ADD WORD');
     }
 
-    // TODO: CLICK EVENT EDITING/UPDATING A WORD
+    // CLICK EVENT EDITING/UPDATING A WORD
     if (e.target.id.includes('edit-word-btn')) {
       console.warn('EDIT WORD', e.target.id);
       console.warn(e.target.id.split('--'));
@@ -25,26 +24,25 @@ const domEvents = () => {
       getSingleWord(firebaseKey).then((wordObj) => addWordForm(wordObj));
     }
 
-    // TODO: CLICK EVENT FOR VIEW WORD DETAILS
+    // CLICK EVENT FOR VIEW WORD DETAILS
     if (e.target.id.includes('view-word-btn')) {
       const [, firebaseKey] = e.target.id.split('--');
 
       getWordDetails(firebaseKey).then(viewWord);
     }
-    // FIXME: ADD CLICK EVENT FOR DELETING AN WORD
-    if (e.target.id.includes('delete-author')) {
-      // eslint-disable-next-line no-alert
+    // ADD CLICK EVENT FOR DELETING AN WORD
+    if (e.target.id.includes('delete-word')) {
       if (window.confirm('Want to delete?')) {
-        console.warn('CLICKED DELETE AUTHOR', e.target.id);
+        console.warn('CLICKED DELETE WORD', e.target.id);
       }
     }
 
-    // FIXME: ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
-    if (e.target.id.includes('add-author-btn')) {
-      console.warn('ADD AUTHOR');
+    //  CLICK EVENT FOR SHOWING FORM FOR ADDING AN WORDS
+    if (e.target.id.includes('add-words-btn')) {
+      console.warn('ADD WORD');
     }
   });
-  // FIXME: ADD CLICK EVENT FOR EDITING AN WORDS
+  // ADD CLICK EVENT FOR EDITING WORDS
   //   if (e.target.id.includes('delete-words-btn')) {
   //   // eslint-disable-next-line no-alert
   //     if (window.confirm('Want to delete?')) {
