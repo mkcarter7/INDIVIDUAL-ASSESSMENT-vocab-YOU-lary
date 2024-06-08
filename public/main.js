0fac13e21f6fdb37a10ea6d9329ebed01923fe62
Melissa Carter <mkd.princess@gmail.com>
2024-06-08 10:14:34 -0500

    clear dom added

--- public/main.js
+++ public/main.js
@@ -1,12 +1,11 @@
 // USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const init = () => {
-
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+// USE WITH FIREBASE AUTH
+  ViewDirectorBasedOnUserAuthStatus();
 };
 
 init();
