3003c88d08969de101734dfb9177c5b1c65c5a34
Melissa Carter <mkd.princess@gmail.com>
2024-06-09 21:59:11 -0500

    worked form events

--- Events/formEvents.js
+++ Events/formEvents.js
@@ -1,20 +1,40 @@
 import { createWord, getWords, updateWord } from '../api/word';
+import { showWords } from '../pages/words';
 
-const formEvents = () => {
+const formEvents = (user) => {
+  console.warn(user);
   document.querySelector('#main-container').addEventListener('submit', (e) => {
     e.preventDefault();
+    if (e.target.id.includes('submit-card')) {
+      const payload = {
+        title: document.querySelector('#word').value,
+        description: document.querySelector('#definition').value,
+        category: document.querySelector('#category').value,
+      };
 
-    // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A WORD
-    if (e.target.id.includes('submit-word')) {
+      createWord(payload, user.uid).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+
+        updateWord(patchPayload).then(() => {
+          getWords(user.uid).then(showWords);
+        });
+      });
       console.warn('CLICKED SUBMIT WORD', e.target.id);
     }
-
-    // CLICK EVENT FOR EDITING A WORD
     if (e.target.id.includes('update-word')) {
       const [, firebaseKey] = e.target.id.split('--');
-      console.warn('CLICKED UPDATE WORD', e.target.id);
-      console.warn(firebaseKey);
+      const payload = {
+        title: document.querySelector('#title').value,
+        description: document.querySelector('#definition').value,
+        category: document.querySelector('#category').value,
+        firebaseKey,
+      };
+
+      updateWord(payload).then(() => {
+        getWords().then(showWords);
+      });
     }
   });
 };
+
 export default formEvents;
