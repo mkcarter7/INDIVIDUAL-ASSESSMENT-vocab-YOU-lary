74810fc4b00d09b66e29c53d0f7bf9fb69b501b4
Melissa Carter <mkd.princess@gmail.com>
2024-06-14 06:42:09 -0500

    cleanup

--- Events/formEvents.js
+++ Events/formEvents.js
@@ -1,7 +1,5 @@
 import { showWords } from '../pages/words';
-import {
-  createWord, getWords, updateWord, getSingleWord
-} from '../api/word';
+import { createWord, getWords, updateWord } from '../api/word';
 
 const formEvents = (user) => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
@@ -11,15 +9,15 @@ const formEvents = (user) => {
       const payload = {
         title: document.querySelector('#title').value,
         definition: document.querySelector('#definition').value,
-        language: document.querySelector('language').value,
+        language: document.querySelector('#language').value,
+        user_id: user
       };
       // this patches the payload object with a firebaseKey and a language
       createWord(payload).then(({ name }) => {
-        getSingleWord(payload.language_id).then((language) => {
-          const patchPayload = { firebaseKey: name, language: language.name };
-          updateWord(patchPayload).then(() => {
-            getWords(user).then(showWords);
-          });
+        const patchPayload = { firebaseKey: name };
+
+        updateWord(patchPayload).then(() => {
+          getWords(user).then((word) => showWords(word));
         });
       });
     }
@@ -28,15 +26,14 @@ const formEvents = (user) => {
     if (e.target.id.includes('update-word')) {
       const [, firebaseKey] = e.target.id.split('--');
       const payload = {
+        title: document.querySelector('#title').value,
         definition: document.querySelector('#definition').value,
         language_id: document.querySelector('#language_id').value,
         time_submitted: Date.now(),
-        title: document.querySelector('#word').value,
-        firebaseKey,
       };
 
-      updateWord(payload).then(() => {
-        getWords(user.uid).then((vocab) => showWords(vocab));
+      updateWord(payload, firebaseKey).then(() => {
+        getWords(user.uid).then((word) => showWords(word));
       });
     }
   });
